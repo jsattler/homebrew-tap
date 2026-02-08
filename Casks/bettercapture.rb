c257bf0e74f1176664979722f7ba12e79404766f
Joshua Sattler <34030048+jsattler@users.noreply.github.com>
2026-02-08 14:00:18 +0100

    chore(bettercapture): bump to 2026.1.1

--- Casks/bettercapture.rb
+++ Casks/bettercapture.rb
@@ -1,6 +1,6 @@
 cask "bettercapture" do
-  version "2026.1.0"
-  sha256 "dc8f58ef6b1168f26602667eedefd7f37e11c14d4cd179ef653fcc6c098ddef6"
+  version "2026.1.1"
+  sha256 "7a240315aa1b466b2c79a5dc0cb7553b84217d3bf9cd4e94a9a400b9aa9112c9"
 
   url "https://github.com/jsattler/BetterCapture/releases/download/v#{version}/BetterCapture-#{version}-arm64.dmg",
       verified: "github.com/jsattler/BetterCapture/"
