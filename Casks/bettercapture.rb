e6e90f7720df9cc383ce042d29f9e6084a259cb8
Joshua Sattler <34030048+jsattler@users.noreply.github.com>
2026-02-08 13:45:02 +0100

    feat: setup bettercapture cask

---
+++ Casks/bettercapture.rb
@@ -0,0 +1,26 @@
+cask "bettercapture" do
+  version "2026.1.0"
+  sha256 "dc8f58ef6b1168f26602667eedefd7f37e11c14d4cd179ef653fcc6c098ddef6"
+
+  url "https://github.com/jsattler/BetterCapture/releases/download/v#{version}/BetterCapture-#{version}-arm64.dmg",
+      verified: "github.com/jsattler/BetterCapture/"
+  name "BetterCapture"
+  desc "The macOS screen recorder you deserve - always free and open source"
+  homepage "https://github.com/jsattler/BetterCapture"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :sequoia"
+  depends_on arch: :arm64
+
+  app "BetterCapture.app"
+
+  zap trash: [
+    "~/Library/Application Support/BetterCapture",
+    "~/Library/Caches/com.sattlerjoshua.BetterCapture",
+    "~/Library/Preferences/com.sattlerjoshua.BetterCapture.plist",
+  ]
+end
